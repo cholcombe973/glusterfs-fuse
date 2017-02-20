8278a0bc1d215477fffec4a9da4c45e50d5cc9fa
Chris Holcombe <xfactor973@gmail.com>
2017-02-20 23:55:44 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,11 @@ use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 
 use fuse::{FileAttr, Filesystem, FileType, Request, ReplyAttr, ReplyDirectory, ReplyEmpty,
-           ReplyEntry, ReplyOpen, ReplyStatfs};
+           ReplyEntry, ReplyOpen, ReplyStatfs, ReplyWrite, ReplyData, ReplyXattr, ReplyCreate,
+           ReplyLock};
 use gfapi_sys::gluster::{Gluster, GlusterDirectory};
 use gfapi_sys::glfs::Struct_glfs_fd;
-use libc::{c_int, c_uchar, DT_REG, DT_DIR, DT_FIFO, DT_CHR, DT_BLK, DT_LNK, ENOSYS};
+use libc::{c_int, c_uchar, DT_REG, DT_DIR, DT_FIFO, DT_CHR, DT_BLK, DT_LNK, ENOENT, ENOSYS};
 use time::Timespec;
 
 const TTL: Timespec = Timespec { sec: 1, nsec: 0 }; // 1 second
@@ -176,9 +177,12 @@ impl Filesystem for GlusterFilesystem {
         println!("lookup(parent={}, name={:?})", _parent, _name);
         println!("lookup current_path: {}",
                  self.current_path(Some(_name.into())).to_string_lossy());
-        let reply_attr = self.stat(&self.current_path(Some(_name.into()))).unwrap();
-        self.inodes.insert(reply_attr.ino, _name.into());
-        reply.entry(&TTL, &reply_attr, 0);
+        if let Ok(reply_attr) = self.stat(&self.current_path(Some(_name.into()))) {
+            self.inodes.insert(reply_attr.ino, _name.into());
+            reply.entry(&TTL, &reply_attr, 0);
+        } else {
+            reply.error(ENOENT);
+        }
     }
     fn readdir(&mut self,
                _req: &Request,
@@ -240,6 +244,228 @@ impl Filesystem for GlusterFilesystem {
     }
     fn statfs(&mut self, _req: &Request, _ino: u64, reply: ReplyStatfs) {
         println!("statfs(ino={})", _ino);
+        reply.error(ENOSYS);
+    }
+    fn setattr(&mut self,
+               _req: &Request,
+               _ino: u64,
+               _mode: Option<u32>,
+               _uid: Option<u32>,
+               _gid: Option<u32>,
+               _size: Option<u64>,
+               _atime: Option<Timespec>,
+               _mtime: Option<Timespec>,
+               _fh: Option<u64>,
+               _crtime: Option<Timespec>,
+               _chgtime: Option<Timespec>,
+               _bkuptime: Option<Timespec>,
+               _flags: Option<u32>,
+               reply: ReplyAttr) {
+        println!("setattr(ino={})", _ino);
+        reply.error(ENOSYS);
+    }
+    fn mknod(&mut self,
+             _req: &Request,
+             _parent: u64,
+             _name: &OsStr,
+             _mode: u32,
+             _rdev: u32,
+             reply: ReplyEntry) {
+        println!("mknod(parent={}, name={:?})", _parent, _name);
+        reply.error(ENOSYS);
+    }
+
+    fn mkdir(&mut self,
+             _req: &Request,
+             _parent: u64,
+             _name: &OsStr,
+             _mode: u32,
+             reply: ReplyEntry) {
+        println!("mkdir(parent={}, name={:?})", _parent, _name);
+        reply.error(ENOSYS);
+    }
+    fn forget(&mut self, _req: &Request, _ino: u64, _nlookup: u64) {
+        println!("forget(ino={:?})", _ino);
+
+    }
+
+    fn readlink(&mut self, _req: &Request, _ino: u64, reply: ReplyData) {
+        println!("readlink(ino={:?})", _ino);
+        reply.error(ENOSYS);
+    }
+
+    fn unlink(&mut self, _req: &Request, _parent: u64, _name: &OsStr, reply: ReplyEmpty) {
+        println!("unlink(name={:?})", _name);
+        reply.error(ENOSYS);
+    }
+
+    fn rmdir(&mut self, _req: &Request, _parent: u64, _name: &OsStr, reply: ReplyEmpty) {
+        println!("rmdir(name={:?})", _name);
+        reply.error(ENOSYS);
+    }
+
+    fn symlink(&mut self,
+               _req: &Request,
+               _parent: u64,
+               _name: &OsStr,
+               _link: &Path,
+               reply: ReplyEntry) {
+        println!("symlink(name={:?})", _name);
+        reply.error(ENOSYS);
+    }
+
+    /// Rename a file.
+    fn rename(&mut self,
+              _req: &Request,
+              _parent: u64,
+              _name: &OsStr,
+              _newparent: u64,
+              _newname: &OsStr,
+              reply: ReplyEmpty) {
+        println!("rename(name={:?})", _name);
+        reply.error(ENOSYS);
+    }
+
+    /// Create a hard link.
+    fn link(&mut self,
+            _req: &Request,
+            _ino: u64,
+            _newparent: u64,
+            _newname: &OsStr,
+            reply: ReplyEntry) {
+        println!("link(ino={:?})", _ino);
+        reply.error(ENOSYS);
+    }
+
+    fn read(&mut self,
+            _req: &Request,
+            _ino: u64,
+            _fh: u64,
+            _offset: u64,
+            _size: u32,
+            reply: ReplyData) {
+        println!("read(ino={:?})", _ino);
+        reply.error(ENOSYS);
+    }
+
+    fn write(&mut self,
+             _req: &Request,
+             _ino: u64,
+             _fh: u64,
+             _offset: u64,
+             _data: &[u8],
+             _flags: u32,
+             reply: ReplyWrite) {
+        println!("write(ino={:?})", _ino);
+        reply.error(ENOSYS);
+    }
+
+    fn flush(&mut self, _req: &Request, _ino: u64, _fh: u64, _lock_owner: u64, reply: ReplyEmpty) {
+        println!("flush(ino={:?})", _ino);
+        reply.error(ENOSYS);
+    }
+
+    fn release(&mut self,
+               _req: &Request,
+               _ino: u64,
+               _fh: u64,
+               _flags: u32,
+               _lock_owner: u64,
+               _flush: bool,
+               reply: ReplyEmpty) {
+        println!("release(ino={:?})", _ino);
+        reply.ok();
+    }
+
+    fn fsync(&mut self, _req: &Request, _ino: u64, _fh: u64, _datasync: bool, reply: ReplyEmpty) {
+        println!("fsync(ino={:?})", _ino);
+        reply.error(ENOSYS);
+    }
+
+    fn fsyncdir(&mut self,
+                _req: &Request,
+                _ino: u64,
+                _fh: u64,
+                _datasync: bool,
+                reply: ReplyEmpty) {
+        println!("fsyncdir(ino={:?})", _ino);
+        reply.error(ENOSYS);
+    }
+
+    /// Set an extended attribute.
+    fn setxattr(&mut self,
+                _req: &Request,
+                _ino: u64,
+                _name: &OsStr,
+                _value: &[u8],
+                _flags: u32,
+                _position: u32,
+                reply: ReplyEmpty) {
+        println!("setxattr(ino={:?})", _ino);
+        reply.error(ENOSYS);
+    }
+
+    fn getxattr(&mut self,
+                _req: &Request,
+                _ino: u64,
+                _name: &OsStr,
+                _size: u32,
+                reply: ReplyXattr) {
+        println!("getxattr(ino={:?})", _ino);
+        reply.error(ENOSYS);
+    }
+
+    fn listxattr(&mut self, _req: &Request, _ino: u64, _size: u32, reply: ReplyXattr) {
+        println!("listxattr(ino={:?})", _ino);
+        reply.error(ENOSYS);
+    }
+
+    fn removexattr(&mut self, _req: &Request, _ino: u64, _name: &OsStr, reply: ReplyEmpty) {
+        println!("removexattr(ino={:?})", _ino);
+        reply.error(ENOSYS);
+    }
+
+    fn access(&mut self, _req: &Request, _ino: u64, _mask: u32, reply: ReplyEmpty) {
+        println!("access(ino={:?})", _ino);
+        reply.error(ENOSYS);
+    }
+
+    fn create(&mut self,
+              _req: &Request,
+              _parent: u64,
+              _name: &OsStr,
+              _mode: u32,
+              _flags: u32,
+              reply: ReplyCreate) {
+        println!("create(name={:?})", _name);
+        reply.error(ENOSYS);
+    }
+
+    fn getlk(&mut self,
+             _req: &Request,
+             _ino: u64,
+             _fh: u64,
+             _lock_owner: u64,
+             _start: u64,
+             _end: u64,
+             _typ: u32,
+             _pid: u32,
+             reply: ReplyLock) {
+        println!("getlk(ino={:?})", _ino);
+        reply.error(ENOSYS);
+    }
+    fn setlk(&mut self,
+             _req: &Request,
+             _ino: u64,
+             _fh: u64,
+             _lock_owner: u64,
+             _start: u64,
+             _end: u64,
+             _typ: u32,
+             _pid: u32,
+             _sleep: bool,
+             reply: ReplyEmpty) {
+        println!("setlk(ino={:?})", _ino);
         reply.error(ENOSYS);
     }
 }
